57279387577dc6896691abaeea520fecdb6eee83
Israel Saeta Pérez <dukebody@gmail.com>
2010-08-17 00:17:52 +0200

    Add i18n message factory.

--- acentoweb/insurance/__init__.py
+++ acentoweb/insurance/__init__.py
@@ -1,4 +1,7 @@
   # -*- extra stuff goes here -*- 
+from zope.i18nmessageid import MessageFactory
+
+_ = MessageFactory("acentoweb.insurance")
 
 def initialize(context):
     """Initializer called when used as a Zope 2 product."""

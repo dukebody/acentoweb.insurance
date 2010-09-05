159ca25e470031663e15913437394c094eb4bc76
Israel Saeta Pérez <dukebody@gmail.com>
2010-09-05 19:49:37 +0200

    Create comparative view.

---
+++ acentoweb/insurance/browser/community_comparative.py
@@ -0,0 +1,13 @@
+from Products.Five.browser import BrowserView
+
+from acentoweb.insurance.vocabulary import YesNoVocabulary
+
+
+class CommunityComparativeView(BrowserView):
+
+    def getTitleFromValue(self, value):
+        """Return the title of the term with the provided value using
+        the YesNoVocabulary.
+        """
+
+        return YesNoVocabulary.getTerm(value).title

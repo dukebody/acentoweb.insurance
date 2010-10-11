16ce46274624b9e5f9cea369b17702be26f1749c
Israel Saeta Pérez <dukebody@gmail.com>
2010-10-11 20:49:01 +0200

    I18nalize workflow.

--- acentoweb/insurance/updatelocales.sh
+++ acentoweb/insurance/updatelocales.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 i18ndude rebuild-pot --pot locales/acentoweb.insurance.pot --create acentoweb.insurance .
 i18ndude sync --pot locales/acentoweb.insurance.pot locales/*/LC_MESSAGES/acentoweb.insurance.po
+i18ndude sync --pot locales/plone.pot locales/*/LC_MESSAGES/plone.po
 
